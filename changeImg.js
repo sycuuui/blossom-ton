6ba7f355dfb313a8d584a8a9532deda89fc3888d
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-02-20 20:21:49 +0900

    이미지 바뀌는 js 추가

---
+++ changeImg.js
@@ -0,0 +1,20 @@
+var imgArray = new Array();
+imgArray[0]="img/1.png";
+imgArray[1]="img/2.png";
+imgArray[2]="img/3.png";
+imgArray[3]="img/4.png";
+
+var count = 0;
+
+function changeImage(){
+        var objImg = document.getElementById("blossom");
+        objImg.src=imgArray[count];
+        count++;
+        if(count>5){
+            count=0;
+        }
+        setTimeout(changeImage,500);
+        
+}
+        
+        
